f91c840ea3d2b793454eb270bf7f92a20503baea
Luis Mauricio Alamo <luismalamoc@gmail.com>
2022-02-19 10:28:46 -0300

    feat: autogenerated project

---
+++ src/main/java/com/luismalamoc/springbootcxfsoapclientwithtls/SpringbootcxfsoapclientwithtlsApplication.java
@@ -0,0 +1,13 @@
+package com.luismalamoc.springbootcxfsoapclientwithtls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcxfsoapclientwithtlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcxfsoapclientwithtlsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luismalamoc/springbootcxfsoapclientwithtls/SpringbootcxfsoapclientwithtlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luismalamoc.springbootcxfsoapclientwithtls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcxfsoapclientwithtlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
